275ea910c691390ff3f384e3ed15df918263485b
David Waters <davidj2012@gmail.com>
2022-04-01 13:14:54 -0700

    Alpha Version -- Base Skeleton Django Project
    Very base Django project setup via Corey Shafer YouTube tutorials. This at least gives us a starting point.

---
+++ django_website_database_project/store/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+# Create your views here.
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'store/home.html', context)
+
+def about(request):
+    return render(request, 'store/about.html', {'title': 'About'})
+
